7539b37e786945ce6e43915dd4b8a232d5730656
Dawid Nosal <dawidnosal95@gmail.com>
2026-04-23 19:18:53 +0200

    Event binding

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Task } from '../../models/task';
 
 @Component({
@@ -12,4 +12,11 @@ export class TaskItemComponent {
   // input() - sposób definiowania wejść komponentu
   // W szablonie odczytujemy wartość jak funkcję: task().title
   task = input.required<Task>();
+
+  toogle = output<void>();
+
+  // Wysłanie zdarzenia do rodzica
+  onClick(): void {
+    this.toogle.emit();
+  }
 }
--- src/app/components/task-list/task-list-component.ts
+++ src/app/components/task-list/task-list-component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { TaskService } from '../../services/task/task-service';
 import { TaskItemComponent } from '../task-item/task-item.component';
 
@@ -18,4 +18,13 @@ export class TaskListComponent {
   // constructor (private taskService: TaskService){ }
 
   readonly tasks = this.taskService.tasks;
+
+  // computed() - wartość jest automatycznie obliczana na podstawie innych signal
+  readonly totalCount = computed(() => this.tasks().length);
+
+  readonly doneCount = computed(() => this.tasks().filter((x) => x.isDone).length);
+
+  onToggle(id: number): void {
+    this.taskService.toogleDone(id);
+  }
 }
